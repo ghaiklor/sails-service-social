de6c6b9a99fcceb7782504426141817d55bfd4dd
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-24 18:58:01 +0300

    Update tests for SocialService

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,21 @@
 var assert = require('chai').assert;
-var FacebookSocial = require('../lib/FacebookSocial');
 var SocialService = require('../');
+var FacebookSocial = SocialService.FacebookSocial;
 
 describe('SocialService', function () {
   it('Should properly export', function () {
     assert.isObject(SocialService);
     assert.isFunction(SocialService.create);
-    assert.isFunction(SocialService.Facebook);
+    assert.isFunction(SocialService.FacebookSocial);
+  });
+
+  it('Should properly create twilio instance', function () {
+    assert.instanceOf(SocialService.create('facebook'), FacebookSocial);
+  });
+
+  it('Should properly throw exception on create unrecognized', function () {
+    assert.throw(function () {
+      SocialService.create('NOT_EXISTS');
+    }, Error);
   });
 });
