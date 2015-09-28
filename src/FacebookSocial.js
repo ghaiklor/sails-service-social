8f371dfaf21669af5062d29d3f1b8c2738e398a5
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 15:03:48 +0300

    Rewrite FacebookSocial with ES6

--- src/FacebookSocial.js
+++ src/FacebookSocial.js
@@ -1,96 +1,79 @@
-var fb = require('fb');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var util = require('util');
-var BaseSocial = require('./BaseSocial');
+import _ from 'lodash';
+import fb from 'fb';
+import BaseSocial from './BaseSocial';
 
-util.inherits(FacebookSocial, BaseSocial);
+export default class FacebookSocial extends BaseSocial {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Facebook class
- * @param {Object} [_config]
- * @constructor
- */
-function FacebookSocial(_config) {
-  BaseSocial.apply(this, arguments);
+    this.setProvider(fb);
+  }
 
-  this.setProvider(fb);
+  /**
+   * Get profile from Facebook
+   * @param {String} [_accessToken] Facebook access token
+   * @param {Object} [_config] Additional config for request
+   * @returns {Promise}
+   */
+  getProfile(_accessToken, _config) {
+    let accessToken = _accessToken || this.get('accessToken') || '';
+    let config = _.merge({fields: ['id', 'name', 'email']}, _.omit(this.get(), 'accessToken'), _config);
+
+    this.getProvider().setAccessToken(accessToken);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().api('me', config, response => response.error ? reject(response.error) : resolve(response));
+    });
+  }
+
+  /**
+   * Get friends from Facebook
+   * @param {String} [_accessToken] Facebook access token
+   * @param {Object} [_config] Additional config for request
+   * @returns {Promise}
+   */
+  getFriends(_accessToken, _config) {
+    let accessToken = _accessToken || this.get('accessToken') || '';
+    let config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
+
+    this.getProvider().setAccessToken(accessToken);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().api('me/friends', config, response => response.error ? reject(response.error) : resolve(response.data));
+    });
+  }
+
+  /**
+   * Get photos from Facebook
+   * @param {String} [_accessToken] Facebook access token
+   * @param {Object} [_config] Additional config for request
+   * @returns {Promise}
+   */
+  getPhotos(_accessToken, _config) {
+    let accessToken = _accessToken || this.get('accessToken') || '';
+    let config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
+
+    this.getProvider().setAccessToken(accessToken);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().api('me/photos', config, response => response.error ? reject(response.error) : resolve(response.data));
+    });
+  }
+
+  /**
+   * Get posts from Facebook
+   * @param {String} [_accessToken] Facebook access token
+   * @param {Object} [_config] Additional config for request
+   * @returns {Promise}
+   */
+  getPosts(_accessToken, _config) {
+    let accessToken = _accessToken || this.get('accessToken') || '';
+    let config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
+
+    this.getProvider().setAccessToken(accessToken);
+
+    return new Promise((resolve, reject) => {
+      this.getProvider().api('me/posts', config, response => response.error ? reject(response.error) : resolve(response.data));
+    });
+  }
 }
-
-/**
- * Get profile from Facebook
- * @param {String} [_accessToken] Facebook access token
- * @param {Object} [_config] Additional config for request
- * @returns {Promise}
- */
-FacebookSocial.prototype.getProfile = function (_accessToken, _config) {
-  var accessToken = _accessToken || this.get('accessToken') || '';
-  var config = _.merge({fields: ['id', 'name', 'email']}, _.omit(this.get(), 'accessToken'), _config);
-
-  this.getProvider().setAccessToken(accessToken);
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider().api('me', config, function (response) {
-      return response.error ? reject(response.error) : resolve(response);
-    });
-  }.bind(this));
-};
-
-/**
- * Get friends from Facebook
- * @param {String} [_accessToken] Facebook access token
- * @param {Object} [_config] Additional config for request
- * @returns {Promise}
- */
-FacebookSocial.prototype.getFriends = function (_accessToken, _config) {
-  var accessToken = _accessToken || this.get('accessToken') || '';
-  var config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
-
-  this.getProvider().setAccessToken(accessToken);
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider().api('me/friends', config, function (response) {
-      return response.error ? reject(response.error) : resolve(response.data);
-    });
-  }.bind(this));
-};
-
-/**
- * Get photos from Facebook
- * @param {String} [_accessToken] Facebook access token
- * @param {Object} [_config] Additional config for request
- * @returns {Promise}
- */
-FacebookSocial.prototype.getPhotos = function (_accessToken, _config) {
-  var accessToken = _accessToken || this.get('accessToken') || '';
-  var config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
-
-  this.getProvider().setAccessToken(accessToken);
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider().api('me/photos', config, function (response) {
-      return response.error ? reject(response.error) : resolve(response.data);
-    });
-  }.bind(this));
-};
-
-/**
- * Get posts from Facebook
- * @param {String} [_accessToken] Facebook access token
- * @param {Object} [_config] Additional config for request
- * @returns {Promise}
- */
-FacebookSocial.prototype.getPosts = function (_accessToken, _config) {
-  var accessToken = _accessToken || this.get('accessToken') || '';
-  var config = _.merge({}, _.omit(this.get(), 'accessToken'), _config);
-
-  this.getProvider().setAccessToken(accessToken);
-
-  return new Promise(function (resolve, reject) {
-    this.getProvider().api('me/posts', config, function (response) {
-      return response.error ? reject(response.error) : resolve(response.data);
-    });
-  }.bind(this));
-};
-
-module.exports = FacebookSocial;
